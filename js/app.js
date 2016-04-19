11f2ee8c473b83a58d60037e93f4708de91eb764
llanginger <llanginger@gmail.com>
2016-04-18 23:36:51 -0700

    placeholder image in place

--- js/app.js
+++ js/app.js
@@ -9,17 +9,11 @@ $(function(){
     $(".wrapper").toggleClass("open");
   });
 
-  // $("#search-now").on("click", function(){
-  //   ViewModel.fourSquareApiCall();
-  // })
+  $("#display-random").on("click", function(){
+    $(".wrapper").toggleClass("open");
+  });
 
 
-
-  var CoffeeShop = function(data){
-    this.name = ko.observable(data.name);
-    this.address = ko.observable(data.address);
-    this.rating = ko.observable(data.rating);
-  };
 
 
   // var fourSquareResults = [];
@@ -32,29 +26,61 @@ $(function(){
     this.searchLocation = ko.observable("ballard, wa");
     this.searchCategory = ko.observable("coffee");
     this.resultName = ko.observable();
+    this.resultId = ko.observable();
     this.resultLimit = ko.observable(10);
     this.fourSquareResults = ko.observableArray([]);
+    this.fourSqPhotos = ko.observableArray([]);
 
     this.selectedItems = ko.observableArray([]);
 
-    this.currentItem = ko.observable();
+    this.currentItem = ko.observable({});
+
+    this.fourSqSettings = ko.observable({
+      baseUrl: "https://api.foursquare.com/v2/venues/",
+      search: "search?",
+      clientID: "client_id=" + "WGMJMEF5PGBY0Z2VPGOTUV4IZWYTZS5V1E0TPIJHBSHRXNWS",
+      clientSecret: "&client_secret=" + "MPIRWAHDMNBZVY2LSAVR1Y0WLQEP5SLDQHIXJZLVFILJHJDQ",
+      loc: "near=" + self.searchLocation(),
+      cat: "&query=" + self.searchCategory(),
+      limit: "&limit=" + self.resultLimit()
+    });
+    var fourSqSearch_URL =
+      self.fourSqSettings().baseUrl + self.fourSqSettings().search + self.fourSqSettings().loc + "&" +  self.fourSqSettings().clientID + self.fourSqSettings().clientSecret + "&v=20130815" + self.fourSqSettings().cat + self.fourSqSettings().limit;
+
+    var fourSqPhoto_URL = self.fourSqSettings().baseUrl + "40b3de00f964a52026001fe3" + "/photos?" + self.fourSqSettings().clientID + self.fourSqSettings().clientSecret + "&v=20130815";
+
 
     this.removeSelected = function(){
       self.fourSquareResults.removeAll(self.selectedItems());
       self.selectedItems([]);
-      console.log(self.fourSquareResults()[0].name);
     }
 
     this.displayRandom = function(){
       var ranNum = Math.floor(Math.random() * self.fourSquareResults().length);
+
+      // Placeholder image code. TODO: replace this
+      var photoSrc = "https://irs0.4sqi.net/img/general/300x200/2341723_vt1Kr-SfmRmdge-M7b4KNgX2_PHElyVbYL65pMnxEQw.jpg/"
+
       self.currentItem(self.fourSquareResults()[ranNum]);
+
       var newLat = self.currentItem().lat;
       var newLng = self.currentItem().lng;
+
       self.resultName(self.currentItem().name);
-      console.log(self.resultName());
-      console.log(self.currentItem());
+      self.resultId(self.currentItem().id);
+      self.fourSqSettings().id = self.currentItem().id;
+
+
+          console.log(self.resultName());
+          console.log(self.currentItem());
+          console.log(self.resultId());
+          console.log(fourSqPhoto_URL);
+          console.log(self.fourSqSettings());
+
       myLatLong = {lat: newLat, lng: newLng};
-      // console.log(myLatLong);
+          // console.log(myLatLong);
+
+
       map.panTo(myLatLong);
       if (!marker) {
         marker = new google.maps.Marker({
@@ -69,43 +95,66 @@ $(function(){
       marker.addListener('click', toggleBounce);
       if (infoWindow) {infoWindow.close()};
       infoWindow = new google.maps.InfoWindow({
-          content: '<div id="content">' + '<h1 id="firstHeading" class="firstHeading">' + self.resultName() + '</h1>' + '</div>'
+          content: '<div id="content">' + "<img src='http://placehold.it/400x250'/>" + '<h1 id="firstHeading" class="firstHeading">' + self.resultName() + '</h1>' + '</div>'
       });
       infoWindow.open(map, marker, this);
+      mapRecenter(myLatLong, 0, -100);
     };
 
+    var mapRecenter = function(latlng,offsetx,offsety) {
+      var point1 = map.getProjection().fromLatLngToPoint(
+          (latlng instanceof google.maps.LatLng) ? latlng : map.getCenter()
+      );
+      var point2 = new google.maps.Point(
+          ( (offsetx) / Math.pow(2, map.getZoom()) ) || 0,
+          ( (offsety) / Math.pow(2, map.getZoom()) ) || 0
+      );
+      map.setCenter(map.getProjection().fromPointToLatLng(new google.maps.Point(
+          point1.x - point2.x,
+          point1.y + point2.y
+      )));
+    }
+
     this.fourSquareApiCall = function(){
+      // Empty results array:
       self.fourSquareResults([]);
-      var fourSqSettings = {
-        baseUrl: "https://api.foursquare.com/v2/venues/search?",
-        clientID: "&client_id=" + "WGMJMEF5PGBY0Z2VPGOTUV4IZWYTZS5V1E0TPIJHBSHRXNWS",
-        clientSecret: "&client_secret=" + "MPIRWAHDMNBZVY2LSAVR1Y0WLQEP5SLDQHIXJZLVFILJHJDQ",
-        loc: "near=" + self.searchLocation(),
-        cat: "&query=" + self.searchCategory(),
-        limit: "&limit=" + self.resultLimit(),
-      };
-
-      var fourSq_URL =
-        fourSqSettings.baseUrl + fourSqSettings.loc + fourSqSettings.clientID + fourSqSettings.clientSecret + "&v=20130815" + fourSqSettings.cat + fourSqSettings.limit;
-
-      $.ajax(fourSq_URL)
+      // Request info from Foursqaure:
+      $.ajax(fourSqSearch_URL)
         .fail(function(data){
           console.log("Failed 4square request");
         })
         .done(function(data){
-          console.log(data);
+          // console.log(data);
           var venues = data.response.venues;
           for (var venue in venues){
-            // console.log(venues[i].name);
             self.fourSquareResults.push({
               name: venues[venue].name,
               lat: venues[venue].location.lat,
-              lng: venues[venue].location.lng
+              lng: venues[venue].location.lng,
+              id: venues[venue].id
             });
-            // console.log(self.fourSquareResults()[venue].name);
           };
         })
       };
+      // this.fourSquarePhotoCall = function(){
+      //
+      //
+      //   // self.currentItem().id = "";
+      //   $.ajax(fourSqPhoto_URL)
+      //     .fail(function(data){
+      //       console.log("Failed 4square photo request");
+      //     })
+      //     .done(function(data){
+      //       var photos = data.response.photos.items;
+      //       for (var photo in photos){
+      //         self.fourSqPhotos.push({
+      //           photoId: photos[photo].id
+      //         });
+      //       }
+      //     })
+      //     console.log(self.fourSqPhotos());
+      //     self.fourSqPhotos([]);
+      // }
   };
 
   ko.applyBindings(new ViewModel);
